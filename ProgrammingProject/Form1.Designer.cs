f5fe2842b3563a76de2469113baa186a77b1e045
HellDreadLord <130399300+HellDreadLord@users.noreply.github.com>
2023-04-20 19:12:49 +0100

    Initial commit

---
+++ ProgrammingProject/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ProgrammingProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            button2 = new Button();
+            trackBar1 = new TrackBar();
+            label1 = new Label();
+            LabelOrbit = new Label();
+            LabelPlanetDay = new Label();
+            LabelDate = new Label();
+            LabelSeason = new Label();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Interval = 50;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(14, 47);
+            button1.Name = "button1";
+            button1.Size = new Size(76, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Stop";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(14, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(76, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Start";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(97, 12);
+            trackBar1.Maximum = 100;
+            trackBar1.Minimum = 1;
+            trackBar1.Name = "trackBar1";
+            trackBar1.RightToLeft = RightToLeft.Yes;
+            trackBar1.Size = new Size(296, 56);
+            trackBar1.TabIndex = 2;
+            trackBar1.Value = 50;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(186, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Planet Velocity";
+            // 
+            // LabelOrbit
+            // 
+            LabelOrbit.AutoSize = true;
+            LabelOrbit.BackColor = Color.Transparent;
+            LabelOrbit.ForeColor = Color.White;
+            LabelOrbit.Location = new Point(3, 94);
+            LabelOrbit.Name = "LabelOrbit";
+            LabelOrbit.Size = new Size(94, 20);
+            LabelOrbit.TabIndex = 4;
+            LabelOrbit.Text = "Orbit Period";
+            // 
+            // LabelPlanetDay
+            // 
+            LabelPlanetDay.AutoSize = true;
+            LabelPlanetDay.BackColor = Color.Transparent;
+            LabelPlanetDay.ForeColor = Color.White;
+            LabelPlanetDay.Location = new Point(6, 124);
+            LabelPlanetDay.Name = "LabelPlanetDay";
+            LabelPlanetDay.Size = new Size(84, 20);
+            LabelPlanetDay.TabIndex = 5;
+            LabelPlanetDay.Text = "Planet Day";
+            // 
+            // LabelDate
+            // 
+            LabelDate.AutoSize = true;
+            LabelDate.BackColor = Color.Transparent;
+            LabelDate.ForeColor = Color.White;
+            LabelDate.Location = new Point(6, 153);
+            LabelDate.Name = "LabelDate";
+            LabelDate.Size = new Size(42, 20);
+            LabelDate.TabIndex = 6;
+            LabelDate.Text = "Date";
+            // 
+            // LabelSeason
+            // 
+            LabelSeason.AutoSize = true;
+            LabelSeason.BackColor = Color.Transparent;
+            LabelSeason.ForeColor = Color.White;
+            LabelSeason.Location = new Point(6, 183);
+            LabelSeason.Name = "LabelSeason";
+            LabelSeason.Size = new Size(58, 20);
+            LabelSeason.TabIndex = 7;
+            LabelSeason.Text = "Season";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(486, 353);
+            Controls.Add(LabelSeason);
+            Controls.Add(LabelDate);
+            Controls.Add(LabelPlanetDay);
+            Controls.Add(LabelOrbit);
+            Controls.Add(label1);
+            Controls.Add(trackBar1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            Paint += Form1_Paint;
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Button button1;
+        private Button button2;
+        private TrackBar trackBar1;
+        private Label label1;
+        private Label LabelOrbit;
+        private Label LabelPlanetDay;
+        private Label LabelDate;
+        private Label LabelSeason;
+    }
+}
---
+++ ProgrammingProject/Form1.cs
@@ -0,0 +1,115 @@
+using System.Drawing.Drawing2D; //Uses the library of System.Drawing for drawing 2D objects.
+
+namespace ProgrammingProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); // Loads the compiled page of Form1.
+        }
+
+        float angle = 0, planetVelocity = 1; // Define constants used to determine movement of items.
+        int dx = 210, dy = 150, distance = 50; // Define constants used to determine positioning of items.
+
+        DateTime date = new DateTime();
+        int days; // Creates a variable for the number of days that has passed
+
+        void UpdateSeason()
+        // Changes the label displaying seasons based on the number of days that have passed
+        {
+            if (days >= 1 && days <= 93)
+            {
+                LabelSeason.Text = "Season: Spring";
+            }
+            else if (days >= 94 && days <= 186)
+            {
+                LabelSeason.Text = "Season: Summer";
+            }
+            else if (days >= 187 && days <= 276)
+            {
+                LabelSeason.Text = "Season: Autumn";
+            }
+            else
+            {
+                LabelSeason.Text = "Season: Winter";
+            }
+
+        }
+        void UpdateOrbitPeriod()
+        // Updates labels based on time passed.
+        {
+            date = date.AddDays(planetVelocity);
+            LabelDate.Text = date.ToString("dd, MM, yyyy"); // Defines the structure the date on the label as day/Month/year.
+            days += 5; // Increments the number of days that have passed by five.
+            if (days >= 364)
+            {
+                LabelOrbit.Text = "Orbit Complete - +1 Year"; // When one year passes, display that the orbit has been completed
+                days = 0; // When a year passes, reset the number of days to 0.
+            }
+            if (days == 10)
+            {
+                LabelOrbit.Text = "Orbiting";
+            }
+            LabelPlanetDay.Text = "Days: " + days.ToString(); // Displays how many days have passed on the label.
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        // Draws the various objects used in the program and determines movement of the objects.
+        {
+            Graphics g = e.Graphics; // Use graphics class when using "System.Drawing"
+            StringFormat str = new StringFormat(); // Initializes a new object as a string.
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; // Smooths the curves to remove pixellated jagged lines.
+            g.TranslateTransform(dx, dy, MatrixOrder.Append); // Moves the origin location of object, appending the change to transformation matrix.
+            g.DrawEllipse(Pens.White, -distance + 3, -distance + 3, 100, 100); // Draws an empty circle within a bounding square of width of 100 and height of 100, using a white pen.
+            g.ResetTransform(); // Resets the transformation matrix.
+
+            g.FillEllipse(Brushes.Yellow, dx + 5, dy - 10, 20, 20); // Draws a yellow circle within a bounding square of width of 100 and height of 100.
+            g.DrawString("Star", Font, new SolidBrush(Color.Yellow), (int)(dx - distance + 50), (int)(dy - distance + 25), str); // Writes text "Star" in yellow in position determined by variable "distance".
+
+            int x = (int)(dx - distance * Math.Sin(angle * Math.PI / 182.5f)); // Creates new integer "x" by using the sine function and pi.
+            int y = (int)(dy - distance * Math.Cos(angle * Math.PI / 182.5f)); // Creates new integer "x" by using the cosine function and pi.
+
+            g.FillEllipse(Brushes.Aqua, (int)x, (int)y, 10, 10); // Draws a light blue circle within a bounding square of width 100 and height of 100.
+            angle -= planetVelocity; // Variable "angle" is equal to itself subtract the variable "planetVelocity".
+
+            g.DrawEllipse(Pens.White, (int)x - 10, (int)y - 10, 30, 30); // Draws an empty circle within a bounding square of width 30 and height 30, using a white pen.
+
+            int x1 = (int)(x + 15 * Math.Sin(angle * Math.PI / 30f)); // Creates new integer "x1" by using the sine function and pi.
+            int y1 = (int)(y + 15 * Math.Cos(angle * Math.PI / 30f)); // Creates new integer "y1" by using the cosine function and pi.
+
+            g.FillEllipse(Brushes.White, (int)x1, (int)y1, 8, 8); // Draws a white circle within a bounding square of width 8 and height 8.
+
+            g.DrawString("Satellite", Font, new SolidBrush(Color.Yellow), (int)(x - distance + 25), (int)(y - distance + 25), str); // Writes text "Satellite" in yellow in position determined by variable "distance".
+            g.ResetTransform(); // Resets the transformation matrix.
+        }
+
+        private void button2_Click_1(object sender, EventArgs e) // When button2 is pressed:
+        {
+            timer1.Start(); // Start the timer.
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // Each instance of the timer ticking:
+        {
+            UpdateOrbitPeriod(); // Call function UpdateOrbitPeriod.
+            UpdateSeason(); // Call function UpdateSeason
+            Invalidate();  // Invalidates the entire surface of the control and causes the control to be redrawn.
+        }
+
+        private void button1_Click(object sender, EventArgs e) // When button1 is pressed:
+        {
+            timer1.Stop(); //Stop the timer.
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e) // When the trackbar is scrolled along:
+        {
+            timer1.Interval = trackBar1.Value; // The interval of the timer ticking equals the current value of the trackbar.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
